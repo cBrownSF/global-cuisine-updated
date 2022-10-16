313d6f06b4a015a4d07f3ed7a5e8362a9354bc08
Cal Brown <calsbrown4@gmail.com>
2022-10-16 01:38:53 -0700

    font and color updated

--- frontend/src/components/root.js
+++ frontend/src/components/root.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import App from './app';
 import "../0_css_reset.css";
+import "./index.css"
 
 const Root = ({ store }) => (
   <Provider store={store}>
